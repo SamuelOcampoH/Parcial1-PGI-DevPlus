e1f58cf372120e55a48373d4b51566a6143846c4
Samuel Ocampo <samuel.ocampoh@uqvirtual.edu.co>
2026-09-23 12:07:32 -0500

    Creando Clase Desarrolador

---
+++ src/main/java/co/edu/uniquindio/Desarrollador.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio;
+
+public class Desarrollador {
+    private String codigo;
+    private String equipoTrabajo;
+    private String nivel;
+    private int cantMaxProyectos;
+    private double tarifaPorDia;
+    private String estado;
+    private boolean disnponible;
+
+
+    public Desarrollador(String)
+}
