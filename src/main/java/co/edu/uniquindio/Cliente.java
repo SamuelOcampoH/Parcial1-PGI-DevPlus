0e93cff037f7c5f9bfc1f0aa5a2c0c4cac5477f7
Samuel Ocampo <samuel.ocampoh@uqvirtual.edu.co>
2026-09-23 00:36:35 -0500

    Finalizacion de la clase Cliente

--- src/main/java/co/edu/uniquindio/Cliente.java
+++ src/main/java/co/edu/uniquindio/Cliente.java
@@ -15,9 +15,57 @@ public class Cliente {
         this.correo = correo;
         this.pais = pais;
         this.esFrecuente = esFrecuente;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getDocumento() {
+        return documento;
+    }
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public String getPais() {
+        return pais;
+
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+
+    }
+    public boolean isEsFrecuente() {
+        return esFrecuente;
+    }
+    public void setEsFrecuente(boolean esFrecuente) {
+        this.esFrecuente = esFrecuente;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente: " + nombre + "| Documento: " + documento + " | Tel: "+ telefono + " | Pais" + pais;
     }
 
 }
