cb9e81f54bc26cbb1e024eb9ee44d4e9a32889e2
Samuel Ocampo <samuel.ocampoh@uqvirtual.edu.co>
2026-09-22 23:16:50 -0500

    Prueba 2 de guardado

--- src/main/java/co/edu/uniquindio/Main.java
+++ src/main/java/co/edu/uniquindio/Main.java
@@ -4,6 +4,6 @@ package co.edu.uniquindio;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
- System.out.print("Probando");
+ System.out.print("Probandoo");
     }
 }
