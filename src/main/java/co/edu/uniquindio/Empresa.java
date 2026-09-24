9a53f96d1ccf99a406f84aa3c8f7d533e6ce4509
MatiasGuapacha <matias.guapachac@uqvirtual.edu.co>
2026-09-23 21:25:13 -0500

    creación de la clase Empresa inicializada

--- src/main/java/co/edu/uniquindio/Empresa.java
+++ src/main/java/co/edu/uniquindio/Empresa.java
@@ -1,4 +1,64 @@
 package co.edu.uniquindio;
 
 public class Empresa {
+
+    private String nombre;
+    private double nit;
+    private int telefono;
+    private String paginaWeb;
+
+
+    public Empresa(String nombre, double nit, int telefono, String paginaWeb) {
+
+
+        this.nombre = nombre;
+        this.nit = nit;
+        this.telefono = telefono;
+        this.paginaWeb = paginaWeb;
+
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNit() {
+        return nit;
+    }
+
+    public void setNit(double nit) {
+        this.nit = nit;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPaginaWeb() {
+        return paginaWeb;
+    }
+
+    public void setPaginaWeb(String paginaWeb) {
+        this.paginaWeb = paginaWeb;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "nombre='" + nombre + '\'' +
+                ", nit=" + nit +
+                ", telefono=" + telefono +
+                ", paginaWeb='" + paginaWeb + '\'' +
+                '}';
+    }
 }
