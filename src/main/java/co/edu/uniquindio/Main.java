5005dd108df42b7cf63cf9ccd41a2ce8439468cf
Samuel Ocampo <samuel.ocampoh@uqvirtual.edu.co>
2026-09-24 12:33:00 -0500

    creando main

--- src/main/java/co/edu/uniquindio/Main.java
+++ src/main/java/co/edu/uniquindio/Main.java
@@ -4,10 +4,14 @@ package co.edu.uniquindio;
 
 
 public class Main {
+    static Cliente [] listaCliente = new Cliente[10];
+    static Desarrollador [] listaDesarrollador = new Desarrollador[10];
+    static Proyecto [] listaProyecto = new Proyecto[10];
+    static ServicioAdicional [] listaServicioAdicional = new ServicioAdicional[10];
 
 
+    public static void main(String[] args) {
 
-    static void main() {
 
         
 
